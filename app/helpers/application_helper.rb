25ed077c787e11e8d7a1ef719b2bbf053d821c24
visaonrails <vijayasankarmail@gmail.com>
2014-07-23 15:04:58 +0530

    dev two changes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,4 @@
-# Methods added to this helper will be available to all templates in the application. b
+# Methods added to this helper will be available to all templates in the application. dev
 module ApplicationHelper
 end
+#dev another
