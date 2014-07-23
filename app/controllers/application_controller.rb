954ca1dfab4be26a0857e80350deb46a94d2647d
visaonrails <vijayasankarmail@gmail.com>
2014-07-23 14:38:04 +0530

    only controller commited not helpers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,6 @@ class ApplicationController < ActionController::Base
   helper :all # include all helpers, all the time
   protect_from_forgery # See ActionController::RequestForgeryProtection for details
 
-  # Scrub sensitive parameters from your log
+  # Scrub sensitive parameters from your log b
   # filter_parameter_logging :password 
 end
